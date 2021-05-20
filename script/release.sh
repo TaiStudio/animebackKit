3da440db4067cc28534222f08d2dbdc41b71c768
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-20 14:57:38 +0200

    update

--- script/release.sh
+++ script/release.sh
@@ -9,6 +9,14 @@ git clone "https://AnimeBack-Bot:$GH_TOKEN@github.com/TaiStudio/animebackKit" an
 cd animebackKit
 npm ci
 
+npm run check
+
+# bail if nothing changed
+if [ "$(git status --porcelain)" = "" ]; then
+  echo "no new content found; goodbye!"
+  exit
+fi
+
 git config user.email animebot.tai.studio@outlook.fr
 git config user.name AnimeBack-Bot
 git add .
---
+++ script/update.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync('./updater.json', {"version": "n"})
