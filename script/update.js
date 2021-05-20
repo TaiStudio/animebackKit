23b608e8da1489e07b5e69c189d6bf80fc81f1b8
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-20 15:07:33 +0200

    update

--- script/update.js
+++ script/update.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const content = 'Some content!'
 
-fs.writeFile('/Users/joe/test.txt', content, err => {
+fs.writeFileSync('./test.txt', content, err => {
     if (err) {
         console.error(err)
         return
