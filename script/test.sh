d42ac72054d5d069d8184cc54cf19a0240e026cc
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-20 16:53:14 +0200

    update

--- script/test.sh
+++ script/test.sh
@@ -25,7 +25,7 @@ npm version minor -m "bump minor to %s"
 git pull --rebase
 git push origin main
 git push origin main --tags
-echo //npm.pkg.github.com/:_authToken=${NPM_AUTH_TOKEN} > .npmrc
+echo //npm.pkg.github.com/:_authToken=${GH_TOKEN} > .npmrc
 npm publish
 npm pack
 node ./script/publish-to-gh.js
