f310e523c70f15ab32a1c5efaf78df932d9758d7
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-20 12:43:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    createWindow: require('./modules/window'),
+    Tray: require('./modules/tray'),
+    Color: require('./modules/colors')
+}
---
+++ modules/window.js
@@ -0,0 +1,18 @@
+class Window{
+    constructor(file, url, width, height, frame, autoHideMenuBar, resize, nodeIntegration, skipTaskBar, iconFile, alwaysOnTop, BackgroundColor, transparent, pos){
+        this.file = file;
+        this.url = url;
+        this.width = width;
+        this.height = height;
+        this.frame = frame;
+        this.autoHideMenuBar = autoHideMenuBar;
+        this.resize = resize;
+        this.nodeIntegration = nodeIntegration;
+        this.skipTaskBar = skipTaskBar;
+        this.iconFile = iconFile;
+        this.alwaysOnTop = alwaysOnTop;
+        this.BackgroundColor = BackgroundColor;
+        this.transparent = transparent;
+        this.pos = pos;
+    }
+}
